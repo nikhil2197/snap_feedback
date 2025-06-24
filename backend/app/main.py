9b3a0a974bd5fa0ef4302212fcfd821adcee5b6a
nikhil2197 <rameshnikhil21@gmail.com>
2025-06-24 16:50:15 +0530

    fixed cors issue for front end

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,7 +28,7 @@ app = FastAPI(title="Design Feedback App")
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://snap-feedback.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
